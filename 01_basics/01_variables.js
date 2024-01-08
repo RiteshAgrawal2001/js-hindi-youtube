36e28a4516ee0b3380fb0ed8bb1eaf66cca6f388
Ritesh Agrawal <89643268+RiteshAgrawal2001@users.noreply.github.com>
2024-01-08 11:25:33 +0000

    added variable files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 133553
+let accountEmail = "riteshagrawal@google.com"
+var acccountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountID = 2 // not allowed 
+
+accountEmail ="hc@hc.com"
+acccountPassword = "222222222"
+accountCity = "Bengaluru"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, acccountPassword, accountCity, accountState])
